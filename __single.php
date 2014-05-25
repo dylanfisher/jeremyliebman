9faef0f14985e12fd644ea83b68953a44b2e671c
dylanfisher <hi@dylanfisher.com>
2014-05-25 14:05:36 -0400

    remove unused files. move scripts to footer.

--- __single.php
+++
@@ -1,6 +0,0 @@
-<?php get_header() ?>
-<?php get_template_part( 'partials/application', 'router' ); ?>
-<?php get_footer() ?>
-
-</body>
-</html>
--- header.php
+++ header.php
@@ -11,9 +11,6 @@
     <link rel="stylesheet" type="text/css" href="<?php echo bloginfo('stylesheet_url'); ?>" />
     <link rel="stylesheet" type="text/css" href="<?php echo get_bloginfo('template_url'); ?>/css/build/minified/application.css" />
     <script src="<?php echo get_bloginfo('template_url'); ?>/js/modernizr-2.6.2.min.js"></script>
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="<?php echo get_bloginfo('template_url'); ?>/js/jquery-1.10.1.min.js"><\/script>')</script>
-    <script src="<?php echo get_bloginfo('template_url'); ?>/js/build/application.js"></script>
     <?php wp_head() // For plugins ?>
   </head>
   <body>
