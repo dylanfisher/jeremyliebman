e634c5298c8d7ca11741c604a1843ea4e35feb9d
dylanfisher <hi@dylanfisher.com>
2014-06-29 14:44:13 -0400

    remove featured image set option

--- partials/module-image_set.php
+++ partials/module-image_set.php
@@ -1,24 +1,11 @@
 <?php
 
   // Image set router
-  //
-  // Check if we want a large image set or a medium image set, and use that partial.
-  // Use include( locate_template() ) so that we have access to variables declared in this file.
 
-  get_field('featured_set') ? $featured_set = true : $featured_set = false;
+  include(locate_template('partials/module-image_set-image_medium.php'));
 
-  if($featured_set){
-
-    include(locate_template('partials/module-image_set-image_large.php'));
-
-  } else {
-
-    include(locate_template('partials/module-image_set-image_medium.php'));
-
-    if(get_field('video')){
-      include(locate_template('partials/module-videos.php'));
-    }
-
+  if(get_field('video')){
+    include(locate_template('partials/module-videos.php'));
   }
 
 ?>
