d3eb0910a184e989bba0fd33ec590f11193dda40
dylanfisher <hi@dylanfisher.com>
2018-08-23 13:00:27 -0400

    Bust javascript cache

--- footer.php
+++ footer.php
@@ -13,7 +13,7 @@
 
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
   <script>window.jQuery || document.write('<script src="<?php echo get_bloginfo('template_url'); ?>/js/jquery-1.10.1.min.js"><\/script>')</script>
-  <script src="<?php echo get_bloginfo('template_url'); ?>/js/build/application.min.js?ver=2"></script>
+  <script src="<?php echo get_bloginfo('template_url'); ?>/js/build/application.min.js?ver=3"></script>
 
   <script>
     var _gaq=[['_setAccount','UA-15084097-1'],['_trackPageview']];
